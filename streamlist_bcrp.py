26b09e944959030f91776faf5727d274b4287126
dereckamesquita <73800721+dereckamesquita@users.noreply.github.com>
2023-06-23 19:51:46 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -1,32 +1,49 @@
-pip install matplotlib
-
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+import requests
 
-# Generar datos aleatorios
-np.random.seed(0)
-x = np.linspace(0, 10, 100)
-y1 = np.random.randn(100)
-y2 = np.random.randn(100)
-y3 = np.random.randn(100)
+from bcrp_scrapper import *
 
-# Crear la aplicación web
-st.title('Gráfico con Streamlit')
-st.subheader('Gráfico de ejemplo')
+"""
+# Welcome to Streamlit!
 
-# Seleccionar las series a graficar
-columnas = st.multiselect('Selecciona las series', ['Serie 1', 'Serie 2', 'Serie 3'])
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-# Graficar las series seleccionadas
-if columnas:
-    fig, ax = plt.subplots()
-    for columna in columnas:
-        if columna == 'Serie 1':
-            ax.plot(x, y1, label='Serie 1')
-        elif columna == 'Serie 2':
-            ax.plot(x, y2, label='Serie 2')
-        elif columna == 'Serie 3':
-            ax.plot(x, y3, label='Serie 3')
-    ax.legend()
-    st.pyplot(fig)
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+data = {'Nombre': ['Juan', 'María', 'Carlos'],
+        'Edad': [25, 30, 35],
+        'Ciudad': ['Lima', 'Bogotá', 'Santiago']}
+
+df = pd.DataFrame(data)
+
+# Mostrar el DataFrame en Streamlit
+df = bcrpscrapper(bcrp)
+st.dataframe(df)
+
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
+    data = []
+
+    points_per_turn = total_points / num_turns
+
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
+
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
