2eee5f85ad252de03e66c09c319294507cb6fbb2
dereckamesquita <73800721+dereckamesquita@users.noreply.github.com>
2023-06-24 22:20:50 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -36,44 +36,4 @@ df = df[::-1]
 # Convertir el índice en una columna
 df['Periodo'] = df.index
 
-# Mostrar el DataFrame en Streamlit
 
-
-# Configurar el gráfico utilizando Altair
-chart = alt.Chart(df).mark_line().encode(
-    x=alt.X('Periodo', sort='descending'),
-    y=alt.Y('Interbancario - Compra', scale=alt.Scale(domain=[3, 4]))
-)
-
-
-# Mostrar el gráfico en Streamlit
-st.altair_chart(chart, use_container_width=True)
-
-
-###
-line = alt.Chart(df).mark_line().encode(
-    x=alt.X('Periodo', sort='descending'),
-    y=alt.Y('Interbancario - Compra', scale=alt.Scale(domain=[3, 4]))
-)
-
-labels = alt.Chart(df).mark_text(align='left', baseline='middle', dx=3).encode(
-    x=alt.X('Periodo', sort='descending'),
-    y=alt.Y('Interbancario - Compra', scale=alt.Scale(domain=[3, 4])),
-    text=alt.Text('Interbancario - Compra', format='.3f'),
-    color=alt.value('white')
-)
-
-
-
-chart = line + labels
-
-st.altair_chart(chart, use_container_width=True)
-
-# Configurar el gráfico utilizando Altair
-chart = alt.Chart(df).mark_line().encode(
-    x=alt.X('Periodo', sort='descending'),
-    y='Interbancario - Compra'
-)
-
-# Mostrar el gráfico en Streamlit
-st.altair_chart(chart, use_container_width=True)
