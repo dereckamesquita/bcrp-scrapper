49f699b440ca020d688a48a241b1ce40b45b9dca
dereckamesquita <73800721+dereckamesquita@users.noreply.github.com>
2023-06-24 22:16:46 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -1,32 +1,3 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-import requests
-response = requests.get("https://raw.githubusercontent.com/dereckamesquita/bcrp-scrapper/main/bcrp_scrapper.py")
-with open("bcrp_scrapper.py", "w") as file:
-    file.write(response.text)
-from bcrp_scrapper import *
-import matplotlib.pyplot as plt
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import warnings
-warnings.filterwarnings("ignore")
-import urllib.request
-from datetime import datetime
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 
 # Mostrar el DataFrame en Streamlit
 df = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/diarias/resultados/PD04637PD/html','2020-05-01','2020-05-05')
