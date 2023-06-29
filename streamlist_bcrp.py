6a5650fb7c47a53a3056ff33abb6253b7b19c7aa
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-06-29 11:42:54 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -29,7 +29,7 @@ st.write("¡Hola", pd.__version__)
 df1 = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/resultados/PN38705PM/html','2018-03-01','2022-05-05').T
 st.dataframe(df1)
 df1 = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/anuales/resultados/PM06103MA/html','2019-03-01','2023-05-05').T
-df1 = df1.applymap('{:,}'.format)
+df1 = df1.apply(pd.to_numeric, errors='coerce')
 
 st.dataframe(df1)
 
