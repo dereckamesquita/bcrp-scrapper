d142d8038faf891b560f4eff106815acb0588b4d
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-06-29 19:05:19 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -30,8 +30,8 @@ st.altair_chart(gra_bcrp(df2), use_container_width=True)
 '''
 
 st.title('Presentación de bcrpscrapper: Descarga fácil de datos del BCRP')
-st.write('¡Bienvenido a mi aplicación de Streamlit!')
-st.write('En esta aplicación, te presentaré mi librería bcrpscrapper, que te permite descargar datos del Banco Central de Reserva del Perú (BCRP) de forma sencilla y conveniente.')
+st.write('Hola, soy Dereck Amesquita')
+st.markdown('[LinkedIn](https://www.linkedin.com/in/dereck-amesquita/)')st.write('En esta aplicación, te presentaré mi librería bcrpscrapper, que te permite descargar datos del Banco Central de Reserva del Perú (BCRP) de forma sencilla y conveniente.')
 st.write('A continuación, puedes ver un ejemplo de cómo utilizar la librería para descargar datos y mostrarlos en un DataFrame:')
 st.subheader('Serie diaria: Tipo de cambio')
 
