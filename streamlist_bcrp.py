fb89c7ea4d91ef44dc409768b1fd1eeec41437c7
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-07-01 21:05:33 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -52,7 +52,7 @@ st.title('✅ Caida de la inflación en el mes de junio')
 
 st.markdown('[Informe INEI (julio)](https://www.linkedin.com/in/dereck-amesquita/)')
 
-df = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/resultados/PN09824PM/html',
+df = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/resultados/PN01273PM/html',
                   '2020-03-01',
                   '2024-08-05').T
 st.code(codediario, language='python')
