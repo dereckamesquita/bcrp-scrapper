573d3381edc707a87ee108d0dd2eba2daad6111c
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-07-04 18:47:29 -0500

    Update bcrp_scrapper.py

--- bcrp_scrapper.py
+++ bcrp_scrapper.py
@@ -147,8 +147,8 @@ def convertir_fechas(df, columna):
             mes = diccionario_meses.get(fecha[2:5], 1)
             anio = int(fecha[5:])
             anio = ajustar_anio(anio)
-        elif fecha[1] == 'T':
-            trimestre = int(fecha[0])
+        elif fecha[0] == 'T':
+            trimestre = int(fecha[1])
             anio = int(fecha[2:])
             anio = ajustar_anio(anio)
             mes = trimestre * 3
