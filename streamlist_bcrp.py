c2e1ac16f7254f16b8ff502cec94a10dc985e08c
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-07-13 20:52:46 -0500

    Update streamlist_bcrp.py

--- streamlist_bcrp.py
+++ streamlist_bcrp.py
@@ -84,9 +84,14 @@ st.write('📌 Adicionalmente te presento una forma de realizar gráficos rapida
 st.write('Te muestro un ejemplo para cada tipo de dato, donde te dejo los códigos necesarios para su réplica')
 ########### NOTICIA
 st.title('🦖 Replicad de noticia: Cotización del dolar cae (Diario Gestión 13 de julio)')
-df = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/resultados/PN01210PM/html',  '2022-08-01', '2024-08-05')
+df = bcrpscrapper('https://estadisticas.bcrp.gob.pe/estadisticas/series/mensuales/resultados/PN01210PM/html',  '2022-08-01', '2024-08-05').T
 df.loc[pd.to_datetime('2023-07-30')] = 3.582 #Estimado segun promedio hasta el 12 de julio.
-
+chart = gra_bcrp_labels(df)
+chart = chart.properties(
+    title=alt.TitleParams(
+        text= 'Evolución del tipo de cambio (S por $',
+       fontSize=20))
+df.index = df.index.strftime('%b %Y')
 st.altair_chart(chart, use_container_width=True)
 st.dataframe(df.tail(8).T)
 
