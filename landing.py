ef47342edab9641657117d09d2976565b11bddf9
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-07-23 19:59:48 -0500

    Update landing.py

--- landing.py
+++ landing.py
@@ -12,8 +12,13 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import bs4
 
-
-
+code = '''
+!wget https://raw.githubusercontent.com/dereckamesquita/bcrp-scrapper/main/bcrp_scrapper.py
+from bcrp_scrapper import *
+'''
+codefind = '''
+bcrp_find('Reservas internacionales', fre = 'D')
+'''
 st.title('✅ Presentación de bcrpscrapper 2.0: Descarga fácil de datos del BCRP')
 st.write('Hola, soy Dereck Amesquita')
 st.markdown('[LinkedIn](https://www.linkedin.com/in/dereck-amesquita/)')
@@ -22,5 +27,8 @@ st.subheader('Absolutamente todas las series 📊')
 st.write('⭐️ No tienes que descargar nada, ni cuadernos colab, ni raros archivos. Simplemente necesitaras ejecutar un comando simple.')
 st.code(code, language='python')
 st.write('📌 Eso es todo, podrás acceder a cualquier serie del Banco Central para que puedas trabajarla.')
-st.write('📌 Adicionalmente te presento una forma de realizar gráficos rapidamente.')
-st.write('Te muestro un ejemplo para cada tipo de dato, donde te dejo los códigos necesarios para su réplica')
+st.title('📌 Nueva función bcrp_find')
+st.write('Te muestro como usarla')
+st.code(codefind, language='python')
+st.dataframe(bcrp_find('Reservas internacionales', fre = 'D'))
+
