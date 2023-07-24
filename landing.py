d723603f25d8400a0891ec1d7ac5efb21d383694
Dereck Kevin Amesquita Pfoccori <73800721+dereckamesquita@users.noreply.github.com>
2023-07-23 20:15:27 -0500

    final

--- landing.py
+++ landing.py
@@ -35,18 +35,22 @@ st.write('📌 Eso es todo, podrás acceder a cualquier serie del Banco Central 
 st.title('📌 Nueva función: bcrp_find')
 
 st.write("Con `bcrp_find`, podrás obtener el código único de una serie económica y las fechas sobre la última actualización de dicha serie.")
-st.write("¿Cómo usarlo? Es muy sencillo:")
-st.write("1. Ingresa una palabra clave relacionada con el tema económico de tu interés.")
-st.write("2. La función buscará en la base de datos y te mostrará los resultados que coincidan con tu búsqueda.")
-st.write("3. Descubre el código único de la serie y las fechas más recientes sobre su última actualización.")
-st.write("Además, si necesitas datos con una frecuencia específica, como mensuales, trimestrales o anuales, simplemente especifica la frecuencia y `bcrp_find` te proporcionará los datos deseados.")
+#st.write("¿Cómo usarlo? Es muy sencillo:")
+#st.write("1. Ingresa una palabra clave relacionada con el tema económico de tu interés.")
+#st.write("2. La función buscará en la base de datos y te mostrará los resultados que coincidan con tu búsqueda.")
+#st.write("3. Descubre el código único de la serie y las fechas más recientes sobre su última actualización.")
+#st.write("Además, si necesitas datos con una frecuencia específica, como mensuales, trimestrales o anuales, simplemente especifica la frecuencia y `bcrp_find` te proporcionará los datos deseados.")
 
 st.write('Te muestro como usarla. No solo obtendras el código unico, tambien las fechas sobre la ultima actualización de la serie.')
 st.code(codefind, language='python')
 st.dataframe(bcrp_find('Reservas internacionales', fre = 'D'))
 ######
 st.title('📌 Ahora basta con el código de la serie')
-st.write('Suponiendo que quieres ver las Reservas Internacionales Netas, tomaras el codigo "PD04650MD".')
+st.write('A partir de ahora, acceder a tus datos económicos favoritos es más fácil que nunca. Simplemente necesitas el código único de la serie para obtener toda la información que deseas.')
+st.write('Por ejemplo, supongamos que quieres ver los datos de las Reservas Internacionales Netas. En este caso, simplemente tomarás el código de la serie, que es "PD04650MD", y lo ingresarás en la función `bcrpscrapper`.')
+st.write('¡Así de sencillo! Con el código de la serie, podrás acceder a los datos que necesitas para tus análisis y decisiones informadas en el ámbito económico. Descubre tendencias, patrones y estadísticas importantes de manera rápida y eficiente con BCRP Scrapper.')
+st.code(codere, language='python')
+
 df = bcrpscrapper('PD04650MD').T
 st.dataframe(df.head(10))
 
